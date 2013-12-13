5871275db95f016b052c5d7e0ec0de450ba46d02
Lukasz Rozek <lukasz.rozek84@gmail.com>
2013-12-13 14:21:18 +0100

    swt hello world

---
+++ swt-jws-client/src/main/java/pl/lrozek/jws/swt/main/Main.java
@@ -0,0 +1,27 @@
+package pl.lrozek.jws.swt.main;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+public class Main {
+
+    public static void main( String[] args ) {
+        Display display = new Display();
+        Shell shell = new Shell( display );
+
+        Text helloWorldTest = new Text( shell, SWT.NONE );
+        helloWorldTest.setText( "Hello World SWT" );
+        helloWorldTest.pack();
+
+        shell.pack();
+        shell.open();
+        while ( !shell.isDisposed() ) {
+            if ( !display.readAndDispatch() )
+                display.sleep();
+        }
+        display.dispose();
+    }
+
+}
